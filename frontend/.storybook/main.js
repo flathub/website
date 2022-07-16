da704ac8dddd6cdd26ffa6ddb02edc7b35c55417
Razze <razzeee@gmail.com>
2022-07-16 03:03:52 +0200

    Add a11y addon

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "storybook-addon-next",
+    "@storybook/addon-a11y",
   ],
   framework: "@storybook/react",
   core: {
